c754a7bb7c61500fd1497dfa09f3fce503de2c6c
James Aworo <james.aworo@outlook.com>
2026-03-13 23:06:45 +0100

    feat: introduce tool interface for installable tools

---
+++ internal/tools/tool.go
@@ -0,0 +1,17 @@
+package tools
+
+import "github.com/jamesawo/mdev/internal/environment"
+
+type Tool interface {
+	Name() string
+
+	IsInstalled(env *environment.Environment) bool
+
+	Install(env *environment.Environment) error
+
+	Configure(env *environment.Environment) error
+
+	Verify(env *environment.Environment) error
+
+	Uninstall(env *environment.Environment) error
+}
