6445157f41ca5f56d964df853fd44cd79b113cbe
James Aworo <james.aworo@outlook.com>
2026-03-14 19:41:27 +0100

    feat: add brew cask support to installer module

--- internal/installer/brew/brew.go
+++ internal/installer/brew/brew.go
@@ -20,6 +20,20 @@ func Uninstall(pkg string) error {
 	return r.Run("brew", "uninstall", pkg)
 }
 
+func InstallCask(pkg string) error {
+
+	r := &runner.CommandRunner{}
+
+	return r.Run("brew", "install", "--cask", pkg)
+}
+
+func UninstallCask(pkg string) error {
+
+	r := &runner.CommandRunner{}
+
+	return r.Run("brew", "uninstall", "--cask", pkg)
+}
+
 func IsInstalled(pkg string) bool {
 
 	cmd := exec.Command("brew", "list", pkg)
