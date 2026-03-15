b5ac86807ad560b1189acec8e22dd6c8632af98b
James Aworo <james.aworo@outlook.com>
2026-03-15 15:34:57 +0100

    refactor(ui): centralize CLI messages and prepare for i18n support

--- internal/ui/messages/messages.go
+++ internal/ui/messages/messages.go
@@ -1,1 +1,17 @@
 package messages
+
+// todo: put all the message across the ui into this constant for now
+const (
+	System                   = "System"
+	DoctorFailed             = "doctor failed"
+	DoctorReport             = "Doctor Report"
+	Mising                   = "missing"
+	Environment              = "Environment"
+	SetupCancelled           = "Setup cancelled"
+	EnvironmentNotConfigured = "Development data location not configured"
+	CreateDataDirectory      = "Create a directory to store development data?"
+	ChooseDataLocation       = "Choose where to store development data"
+	InstallAllTools          = "Install all tools"
+	InstallTool              = "Install tool"
+	SelectTools              = "Select tools to install"
+)
