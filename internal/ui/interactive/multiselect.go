0530be0ff09842d1762a79fdc5b745b75835496f
James Aworo <james.aworo@outlook.com>
2026-03-15 08:26:38 +0100

    feat(ui): add interactive multiselect using survey

---
+++ internal/ui/interactive/multiselect.go
@@ -0,0 +1,25 @@
+package interactive
+
+import (
+	"github.com/AlecAivazis/survey/v2"
+)
+
+// MultiSelect presents a checkbox selection list
+// and returns the selected option values.
+//
+// Example usage:
+// options := []string{"sdkman","java","maven"}
+// selected, _ := MultiSelect("Select tools", options)
+func MultiSelect(message string, options []string) ([]string, error) {
+
+	var selected []string
+
+	prompt := &survey.MultiSelect{
+		Message: message,
+		Options: options,
+	}
+
+	err := survey.AskOne(prompt, &selected)
+
+	return selected, err
+}
